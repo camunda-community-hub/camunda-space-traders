1cabe5d8c22053518d1859242ef0b60579ef5909
Philipp Ossler <philipp.ossler@gmail.com>
2024-02-01 15:07:12 +0100

    refactor: extract registering a new agent
    Extract the registration of a new agent from the contract fulfillment test. We want to create only one new agent for all test runs.

--- tests/src/test/kotlin/org/camunda/community/spacetraders/ContractsApiTest.kt
+++ tests/src/test/kotlin/org/camunda/community/spacetraders/ContractsApiTest.kt
@@ -1,11 +1,11 @@
 package org.camunda.community.spacetraders
 
 import io.camunda.zeebe.client.ZeebeClient
-import io.camunda.zeebe.process.test.api.ZeebeTestEngine
 import io.camunda.zeebe.spring.test.ZeebeSpringTest
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.entry
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.junit.jupiter.MockitoExtension
 import org.springframework.beans.factory.annotation.Autowired
@@ -22,16 +22,47 @@ import java.util.UUID
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ZeebeSpringTest
 @ExtendWith(MockitoExtension::class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ContractsApiTest {
 
     @Autowired lateinit var client: ZeebeClient
 
-    @Test
-    fun `should fulfill contract`() {
+    var currentToken: String? = null
+
+    fun getToken(): String {
+        return currentToken ?: registerAgent()
+    }
+
+    fun registerAgent(): String {
         // given
         val uuid = UUID.randomUUID().toString().substring(0, 8)
         val agentSymbol = "CST_$uuid"
 
+        client.newDeployResourceCommand()
+                .addResourceFromClasspath("register-agent.bpmn")
+                .send()
+                .join()
+
+        // when
+        val processInstanceResult = client.newCreateInstanceCommand()
+                .bpmnProcessId("register-agent")
+                .latestVersion()
+                .variables(mapOf("agent_symbol" to agentSymbol))
+                .withResult()
+                .send()
+                .join()
+
+        // then
+        assertThat(processInstanceResult.variablesAsMap).containsKey("token")
+
+        val newToken = processInstanceResult.variablesAsMap["token"] as String
+        currentToken = newToken
+        return newToken
+    }
+
+    @Test
+    fun `should fulfill contract`() {
+        // given
         client.newDeployResourceCommand()
                 .addResourceFromClasspath("test-fulfill-contract.bpmn")
                 .send()
@@ -41,7 +72,7 @@ class ContractsApiTest {
         val processInstanceResult = client.newCreateInstanceCommand()
                 .bpmnProcessId("test-fulfill-contract")
                 .latestVersion()
-                .variables(mapOf("agent_symbol" to agentSymbol))
+                .variables(mapOf("token" to getToken()))
                 .withResult()
                 .send()
                 .join()
